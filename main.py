20a3da00bd0d124f4fc9cba3f8002d7b234681f1
nhanna95 <81456125+nhanna95@users.noreply.github.com>
2021-07-15 15:01:30 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 import requests
 import json
 import urllib.request
+import shutil
 from PIL import Image
 
-raw_response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key=6ZbAOVVP4E5zhVRFZipWUJx6M0pR6AahfZqHmdhR').text
-response = json.loads(raw_response)
-explanation = response['explanation']
-hdurl = response['hdurl']
+#funcionality 3
+def main(img_date):
+    if img_date == "":
+        raw_response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key=6ZbAOVVP4E5zhVRFZipWUJx6M0pR6AahfZqHmdhR').text
+    else:
+        raw_response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key=6ZbAOVVP4E5zhVRFZipWUJx6M0pR6AahfZqHmdhR&date={img_date}').text  
+    
+    response = json.loads(raw_response)
+    print(response)
+    explanation = response['explanation']
+    hdurl = response['hdurl']
 
-urllib.request.urlretrieve(hdurl,"apod.png")
-img = Image.open("apod.png")
-img.show()
-print(explanation)
+    urllib.request.urlretrieve(hdurl,"apod.png")
+    img = Image.open("apod.png")
+    img.show()
+    print(explanation)
+
+    #functionality 2
+    r = requests.get(hdurl, stream=True)
+    file = hdurl.split("/")[-1]
+    if r.status_code == 200:
+        r.raw.decode_content = True
+
+        with open(file, 'wb') as f:
+            shutil.copyfileobj(r.raw, f)
+
+
+main("")
+main("2005-11-22")
